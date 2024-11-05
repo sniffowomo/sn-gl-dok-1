c0a6e85d983aab2b9238bdd24f871265b02ab603
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2024-11-05 11:26:25 +0000

    🫦

--- DOK/e426.sh
+++ DOK/e426.sh
@@ -8,14 +8,14 @@ BL='\e[0;34m'
 NC='\e[0m' # No Color
 
 # Variables
-cm1='docker container run -d --name ng nginx:latest'
-cm2='docker container run -d --name my -e MYSQL_RANDOM_ROOT_PASSWORD=true mysql:latest'
+cm1='docker container run -it --name proxy nginx:latest bash'
+
 # commands
 b1() {
     clear
     tx1='
 ----------------------------------------------------------------------
-24. Whats going on inside containers
+26. Getting shell inside a container
 ----------------------------------------------------------------------'
     echo -e "${GR}${tx1}${NC}"
 
@@ -24,11 +24,9 @@ b1() {
 e1() {
     echo -e "${RE}Executing command...${NC}"
     echo -e "${RE} $cm1 ${NC}"
-    echo -e "${RE} $cm2 ${NC}"
     echo -e "${GR} ---START-START-START-START-START------ ${NC}"
     $cm1
     echo -e "${BL} --------------------------------- ${NC}"
-    $cm2
     echo -e "${RE} ---DONE-DONE-DONE-DONE------ ${NC}"
 }
 
